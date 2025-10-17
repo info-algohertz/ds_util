d842d476d2e2169f54cd4ca03115ce9c2fdfcf7c
Dávid Natingga <info@algohertz.com>
2025-10-17 12:23:23 +0200

    Update CSV.rs

--- ds_util/src/csv.rs
+++ ds_util/src/csv.rs
@@ -1,39 +1,47 @@
 use crate::dataframe::DataFrame;
 use std::collections::HashMap;
 
-pub fn read_csv(path: &str) -> Result<Box<dyn DataFrame>, Box<dyn std::error::Error>> {
+const SEPARATOR: chat = ',';
+
+//TODO
+pub fn read_csv(path: &str, column_names: Option<Vec<String>>) -> Result<Box<dyn DataFrame>, Box<dyn std::error::Error>> {
     Ok(Box::new(CsvDataFrame {
         path: path.to_string(),
     }))
 }
 
+//TODO
 struct CsvDataFrame {
     path: String,
+    column_data: HashMap<String, Vec<String>>,
 }
 
 impl DataFrame for CsvDataFrame {
+    //TODO
     fn shape(&self) -> (usize, usize) {
         panic!("Not implemented!");
-        //0usize, 0usize
     }
 
+    //TODO
     fn column_names(&self) -> Vec<String> {
         panic!("Not implemented!");
-        //Vec::new()
     }
 
     fn column_types(&self) -> HashMap<String, String> {
         panic!("Not implemented!");
-        //HashMap::new()
     }
 
+    //TODO
+    fn read_column_i64(&self, column_name: &str) -> Vec<f64> {
+        panic!("Not implemented!");
+    }
+
+    //TODO
     fn read_column_f64(&self, column_name: &str) -> Vec<f64> {
         panic!("Not implemented!");
-        //Vec::new()
-    }
+    }        
 
     fn read_index_microsecond(&self) -> Vec<i64> {
         panic!("Not implemented!");
-        //Vec::new()
     }
 }
