f062ef798b7ed6431972afa7e19b3982ff7bd9c0
Dávid Natingga <info@algohertz.com>
2025-11-19 09:44:11 +0100

    Make DataFrame public

--- ds_util/src/lib.rs
+++ ds_util/src/lib.rs
@@ -6,6 +6,7 @@ mod dataframe;
 mod parquet;
 mod stats;
 
+pub use dataframe::DataFrame;
 pub use csv::read_csv;
 pub use parquet::{read_parquet, write_parquet};
 pub use stats::{get_percentile, get_percentiles, get_mean, get_corr};
